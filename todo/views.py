1737a63ce84d3b648eb5208adbbf6e1133cc8dfa
kimono-dell <alberto.gruning.zen@gmail.com>
2020-11-07 17:46:03 -0300

    cap66-login

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,16 @@
+from django.http import request
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request,'todo/home.html')
 
 def signupuser(request):
     if request.method == 'GET':
-        return render(request,'todo/signupuser.html', {'form':UserCreationForm()})
+        return render(request,'todo/signupuser.html', {'form':AuthenticationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -22,6 +23,17 @@ def signupuser(request):
 
         else:
             return render(request,'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords do not match'})
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request,'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username= request.POST['username'],password = request.POST['password'] )
+        if user is None:
+            return render(request,'todo/loginuser.html', {'form':AuthenticationForm(),'error':'user not found'})
+        else:
+                login(request,user)
+                return redirect('currenttodos')
 
 def logoutuser(request):
     if request.method == 'POST':
--- todoDjango/urls.py
+++ todoDjango/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     #auth
     path('signup/',views.signupuser, name='signupuser'),
     path('logout/',views.logoutuser, name='logoutuser'),
+    path('login/',views.loginuser, name='loginuser'),
     #todos
     path('',views.home, name='home'),
     path('current/',views.currenttodos, name='currenttodos'),
