8518d4da79b17a3dcdeb93f0bf12ee2c2047b5c1
kimono-dell <alberto.gruning.zen@gmail.com>
2020-11-07 18:24:11 -0300

    cap67-todoModel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from .models import Todo
 
-# Register your models here.
+# def TodoAdmin(admin.ModelAdmin)
+#     readonly_fields=('created',)
+
+#admin.site.register(Todo, TodoAdmin)
+admin.site.register(Todo)
